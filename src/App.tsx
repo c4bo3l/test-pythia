b94769a9485c74cd47b71bb8d11a7f39f33e91e8
Andrianto Dwi Laksono <andrianto.dl@gmail.com>
2024-08-09 08:56:08 +0800

    Add Test Pythia page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <iframe src='https://dev.pythia.legal/wl/matter/1573707/forms' style={{
+      width: '100%',
+      height: 'calc(100vh - 30px)'
+    }} />
+  )
+}
+
+export default App
